427a86b4faee3a7dec91bd0b728f127556c9d61f
Jeon-Seho <ho10080@naver.com>
2026-08-12 15:28:47 +0900

    feat: Dockerfile 및 .dockerignore, Docker Compose 작성 및 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,6 +10,7 @@ function apiOrigin() {
 }
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.0.108"],
   env: {
     NEXT_PUBLIC_KAKAO_JS_KEY:
